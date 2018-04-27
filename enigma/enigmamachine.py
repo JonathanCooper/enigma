7725926bb3d6ff87bdf165dfbdacb89285f0e8f9
Jonathan Cooper <jonathan.cooper@onepeloton.com>
2018-04-26 21:57:02 -0400

    fix shit

--- enigma/enigmamachine.py
+++ enigma/enigmamachine.py
@@ -10,7 +10,6 @@ class EnigmaMachine(object):
     def __init__(self, key):
         if len(key) != 3:
             raise Exception('Key length must be 3')
-        #offsets = [ self.letter_to_position(c) for c in key ]
         self.rotors = {
             1: Rotor(self.rotor_one_alphabet),
             2: Rotor(self.rotor_two_alphabet),
@@ -22,7 +21,6 @@ class EnigmaMachine(object):
             rotor = self.rotors[i + 1]
             offset = self.letter_to_position(key[i])
             rotor.rotate(offset)
-            #print(self.rotors[rotor_idx])
             
     def letter_to_position(self, c):
         '''convert letter to its 0-indexed position in the alphabet'''
@@ -31,26 +29,16 @@ class EnigmaMachine(object):
     def decipher(self, ciphertext):
         plaintext = ''
         for character in ciphertext:
-            #print(self.rotors[3])
             self.rotors[3].rotate(1)
-            #print(self.rotors)
-            #print(character)
             for rotor_idx in [3 ,2, 1, 'R']:
-                #print(character)
                 character = self.rotors[rotor_idx].transform_char(character)
-                #print(character)
-            for rev_rotor_idx in [1, 2, 3]:
+                for rev_rotor_idx in [1, 2, 3]:
                 character = self.rotors[rev_rotor_idx].transform_char(
                     character,
                     True
                 )
-                #print(character)
             plaintext += character
-            #print()
         return plaintext
-            
-    #def step(self):
-    #    if self.rotors[2].input_alphabet[0
-
+    
     def __repr__(self):
         return str(self.rotors)
